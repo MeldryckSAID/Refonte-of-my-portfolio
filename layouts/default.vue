6fb80ca5ee516373396116f0119b82c2f7037f57
MeldryckSAID <meldryck.said@edu.univ-fcomte.fr>
2023-11-25 09:00:12 +0100

    feat(master):install

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <main class="layout">
+    <header class="layout__header"></header>
+
+    <NuxtPage />
+    <footer></footer>
+  </main>
+</template>
+<style lang="scss" scoped>
+.layout {
+  display: flex;
+  flex-direction: column;
+}
+</style>

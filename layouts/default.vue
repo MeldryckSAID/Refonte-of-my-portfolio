316efb086d716ae8eb123f3478f1b4e029a72c40
MeldryckSAID <meldryck.said@edu.univ-fcomte.fr>
2023-11-26 18:47:24 +0100

    feat(work)header fixed and css

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,12 @@
 <template>
   <main class="layout">
-    <header class="layout__header"></header>
-
-    <NuxtPage />
+    <div class="header" :class="{ 'header--fixed': isHeaderFixed }">
+      <header class="layout__header">
+        <MyHeader />
+      </header>
+    </div>
+    <div class="cache"></div>
+    <NuxtPage class="main-content" />
     <footer></footer>
   </main>
 </template>
@@ -11,4 +15,43 @@
   display: flex;
   flex-direction: column;
 }
+
+.cache {
+  width: 30px;
+  height: 132px;
+}
+.main-content {
+  margin-top: 10%;
+}
+.header {
+  width: 100%;
+  position: fixed;
+  padding-top: 10px;
+  top: 0;
+  background-color: $L;
+  z-index: 1000;
+}
+
+.header--fixed {
+  position: fixed;
+  background-color: $L;
+}
 </style>
+
+<script setup>
+import { ref, onMounted, onUnmounted } from "vue";
+
+const isHeaderFixed = ref(false);
+
+const handleScroll = () => {
+  isHeaderFixed.value = window.scrollY > 0;
+};
+
+onMounted(() => {
+  window.addEventListener("scroll", handleScroll);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("scroll", handleScroll);
+});
+</script>
---
+++ pages/Projets.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>hello world its projet</h1>
+ 
+</template>
+<style lang="scss">
+h1 {
+  font-family: $primary-font;
+}
+</style>
+<script setup></script>

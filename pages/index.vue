e99ecfe4acb06766c60681e40b6c8718e4b51d80
MeldryckSAID <meldryck.said@edu.univ-fcomte.fr>
2023-11-27 09:20:42 +0100

    feat(work): Fin de css de MyCard et de ces variants

--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,13 @@
     Pourrez-vous comprendre toute ça complicité  Le défi 24 heures est événement phares du département MMI Montbéliard ..."
     image="/png/24h.png"
   />
+  <MyCard
+    id="2"
+    paragraphe="Le défi 24 heures était un défie plein d'émotion incroyable et intrépide 
+    Pourrez-vous comprendre toute ça complicité  Le défi 24 heures est événement phares du département MMI Montbéliard ..."
+    image="/png/rosace.png"
+    :isReversed="true"
+  />
 </template>
 <style lang="scss"></style>
 <script setup></script>
